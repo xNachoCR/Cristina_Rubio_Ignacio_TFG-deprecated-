615fc83c721feb65020eebe048d2230a68292832
xNachoCR <nacho.crubio@gmail.com>
2023-04-12 20:29:58 +0200

    Diseño login completo

--- app/src/main/java/com/example/cristina_rubio_ignacio_tfg/MainActivity.kt
+++ app/src/main/java/com/example/cristina_rubio_ignacio_tfg/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //Prueba development
+
     }
 
 }

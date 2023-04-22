51e274f97b27649a5ff32f5acc529c3dc7b4682d
xNachoCR <nacho.crubio@gmail.com>
2023-04-22 18:58:19 +0200

    Version post menú sidebar

--- app/src/main/java/com/example/cristina_rubio_ignacio_tfg/MainMenuActivity.kt
+++ app/src/main/java/com/example/cristina_rubio_ignacio_tfg/MainMenuActivity.kt
@@ -1,6 +1,5 @@
 package com.example.cristina_rubio_ignacio_tfg
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cristina_rubio_ignacio_tfg.databinding.ActivityMainBinding
